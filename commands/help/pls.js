e1ee970d7856ce93715474638d49add8bfa4ee99
Testy1k <146441942+Testy1k@users.noreply.github.com>
2024-01-26 15:54:17 +0530

    Add files via upload

---
+++ commands/help/pls.js
@@ -0,0 +1,23 @@
+const { Client, MessageEmbed } = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: 'pls',
+  description: 'Show support message',
+  
+  execute(message, usedPrefix) {
+   
+
+    const embed = new MessageEmbed()
+      .setTitle(' Cheers for our staff!')
+      .setDescription(` 🌟Share the love with +vouch @user in the vouching channel <#${config.vouchChannelId}>. Your appreciation brightens our day!\n
+    If you're not satisfied, type -vouch @user to provide feedback. 🎉`)
+      .setColor('#00ff00'); // Green color
+
+   
+   
+    
+
+    message.channel.send(embed);
+  },
+};
